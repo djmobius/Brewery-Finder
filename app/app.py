523bcc1211d2956d0368b0d6b2945805bc2c001d
djmobius <davemobley@gmail.com>
2024-08-08 19:41:29 -0400

    changed to ask for zip code instead of city

--- app/app.py
+++ app/app.py
@@ -10,7 +10,16 @@ def home():
 @app.route('/get_breweries', methods=['POST'])
 def get_breweries():
     state = request.form['state']
-    response = requests.get(f'https://api.openbrewerydb.org/breweries?by_state={state}')
+    postal_code = request.form.get('postal_code')
+    brewery_type = request.form.get('brewery_type')
+
+    url = f'https://api.openbrewerydb.org/breweries?by_state={state}'
+    if postal_code:
+        url += f'&by_postal={postal_code}'
+    if brewery_type:
+        url += f'&by_type={brewery_type}'
+
+    response = requests.get(url)
     breweries = response.json()
     return render_template('breweries.html', breweries=breweries, state=state)
 

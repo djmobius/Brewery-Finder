8dac0734a858b8b05995f2e64786514299b37350
djmobius <davemobley@gmail.com>
2024-08-10 17:14:52 -0400

    added pytest

---
+++ app/tests/test_app.py
@@ -0,0 +1,20 @@
+import pytest
+from app import app
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
+
+def test_homepage(client):
+    """Test the homepage."""
+    response = client.get('/')
+    assert response.status_code == 200
+    assert b'Brewery Finder App' in response.data
+
+def test_get_breweries(client):
+    """Test the brewery search with state parameter."""
+    response = client.post('/get_breweries', data={'state': 'california'})
+    assert response.status_code == 200
+    assert b'10 Barrel Brewing Co' in response.data
